caf7461826e29c152ac164edb37b4deaf955c638
Christopher Fox <cfox1290@gmail.com>
2018-03-01 14:29:44 -0700

    added functionality

--- src/Game/states/load.js
+++ src/Game/states/load.js
@@ -1,5 +1,19 @@
-export default function loadState(){
-    return{
-        
+export default function loadState() {
+    return {
+        preload: function () {
+            var loadingLabel = this.game.add.text(80, 150, 'loading...',
+                { font: '30px Courier', fill: '#fff' });
+
+            this.game.scale.scaleMode = window.Phaser.ScaleManager.NO_SCALE;
+            this.game.scale.pageAlignHorizontally = true;
+            this.game.scale.pageAlignVertically = true;
+
+            this.game.load.image('bullet', 'img/bullets.png');
+            this.game.load.image('ship', 'img/ship.png');
+        },
+        create: function () {
+            this.game.state.start('play');
+        }
+
     }
 }
